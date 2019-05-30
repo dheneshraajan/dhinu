5b95dd0ccbfe86cc76e48e19323df015657852a4
dheneshraajan <51105806+dheneshraajan@users.noreply.github.com>
2019-05-30 22:32:09 +0530

    Update largest number.py

--- largest number.py
+++ largest number.py
@@ -1,7 +1,7 @@
-a,b,c=list(map(int,input().split()))
-if(a>b and a>c):
-    print(a)
-elif(b>a and b>c):
-    print(b)
+k,d,l=list(map(int,input().split()))
+if(k>d and k>l):
+    print(k)
+elif(d>k and d>l):
+    print(d)
 else:
-    print(c)
+    print(l)
