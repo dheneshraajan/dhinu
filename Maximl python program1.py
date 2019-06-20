4927041eb237c5e7aa10f9f7ae10ce2f99a372b1
dheneshraajan <51105806+dheneshraajan@users.noreply.github.com>
2019-06-20 12:15:09 +0530

    Update Maximl python program1.py

--- Maximl python program1.py
+++ Maximl python program1.py
@@ -1,29 +1,28 @@
-
-NO_OF_CHARS=256
-def maximum_distinct_char(str, n): 
-    count=[0]*NO_OF_CHARS   
-    for i in range(n): 
-        count[ord(str[i])] += 1  
+Total=256
+def maximum_distinct_char(str, S): 
+    cnt=[0]*Total   
+    for i in range(S): 
+        cnt[ord(str[i])] += 1  
     maximum_distinct = 0
-    for i in range(NO_OF_CHARS): 
-        if (count[i] != 0): 
+    for i in range(Total): 
+        if (cnt[i] != 0): 
             maximum_distinct += 1    
     return maximum_distinct 
-def smallesteSubstr_maxDistictChar(str): 
-    n = len(str)
-    max_distinct = maximum_distinct_char(str, n) 
-    minsub= n      
-    for i in range(n): 
-        for j in range(n): 
+def smlSubStr_maxDistictChar(str): 
+    S=len(str)
+    max_distinct = maximum_distinct_char(str, S) 
+    minsub=S     
+    for i in range(S): 
+        for j in range(S): 
             subs=str[i:j] 
             subs_length=len(subs) 
             sub_distinct_char=maximum_distinct_char(subs,subs_length) 
               
-            if (subs_length < minsub and 
-                max_distinct == sub_distinct_char): 
+            if (subs_length<minsub and 
+                max_distinct==sub_distinct_char): 
                 minsub=subs_length 
     return minsub 
 if __name__=="__main__":    
     str =input()    
-    answer=smallesteSubstr_maxDistictChar(str); 
+    answer=smlSubStr_maxDistictChar(str); 
     print(answer)
